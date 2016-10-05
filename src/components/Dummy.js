4a55ed20a1ef4207afe1c066701883c5780a07b4
Martin Betak <mbetak@redhat.com>
2016-10-05 10:48:28 +0200

    Add basic npm skeleton project

---
+++ src/components/Dummy.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Dummy () {
+  return (
+    <div>
+      Hello World
+    </div>
+  )
+}
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+function external (root, module) {
+  return {
+    root,
+    commonjs: module,
+    commonjs2: module,
+    amd: module
+  }
+}
+
+module.exports = {
+  entry: {
+    'ovirt-ui-components': './src/index.js',
+  },
+  output: {
+    path: './dist',
+    filename: '[name].js',
+    library: 'OvirtUI',
+    libraryTarget: 'umd',
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, loader: 'babel', exclude: /node_modules/ },
+    ]
+  },
+  resolve: {
+    extensions: ['', '.js'],
+  },
+  externals: [
+    external('React', 'react'),
+    external('ReactDOM', 'react-dom'),
+    external('ReactRedux', 'react-redux'),
+    external('Redux', 'redux'),
+    external('ReduxSaga', 'redux-saga'),
+  ],
+}
