3c7b83a0e8c3bf7f1824d651bdbd7ce927c2fbee
Bohdan <cyberbond95@gmail.com>
2017-04-20 10:42:09 +0200

    Add options modal window

---
+++ src/components/Options/index.js
@@ -0,0 +1,1 @@
+export { default as ConsoleOptions } from './ConsoleOptions'
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ export { default as VmDisks } from './components/VmDisks'
 
 export { default as VmIcon } from './components/VmIcon'
 export { default as VmStatusIcon } from './components/VmStatusIcon'
+export { default as Checkbox } from './components/Checkbox'
 
+export * from './components/Options'
 export * from './vm-status'
 export * from './helpers'
