f88a87a2f0d288e2d61bb3c3376dca47210435a1
Marek Libra <mlibra@redhat.com>
2017-06-15 08:45:29 +0200

    Remove VmDisks

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-export { default as VmDisks } from './components/VmDisks'
-
 export { default as VmIcon } from './components/VmIcon'
 export { default as VmStatusIcon } from './components/VmStatusIcon'
 export { default as Checkbox } from './components/Checkbox'
