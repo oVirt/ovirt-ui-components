bc8d4f32e027fbef7abc0c1bb817c5d028b3fe0e
Marek Libra <mlibra@redhat.com>
2018-05-15 13:41:11 +0200

    Move "<VmIcon>" component to ovirt-web-ui
    Reason to keep it here passed.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-export { default as VmIcon } from './components/VmIcon'
 export { default as VmStatusIcon } from './components/VmStatusIcon'
 export { default as Checkbox } from './components/Checkbox'
 
